34c3806c0368e3441dcfae0ce7c402511bab35c8
moscvinandreea <70642112+moscvinandreea@users.noreply.github.com>
2021-01-12 20:33:05 +0200

    Add files via upload

---
+++ problema 2.py
@@ -0,0 +1,14 @@
+"Se dau două numere. Să se înmulţească cel mai mare cu doi şi cel mai mic cu trei şi să se afişeze rezultatele. Exemplu : date de intrare « numere.txt »: 3  7  date de ieşire « produs.txt »: 9  14"
+
+with open ("umetr.txt", "r") as f:
+    a=f.readline()
+    b=f.readline()
+if int(a)>int(b):
+    x=int(a)**2
+    y=int(b)**3
+if int(b)>int(a):
+    x=int(b)**2
+    y=int(a)**3
+with open ("produs.txt","w") as f:
+    f.write(str(x))
+    f.write(str(y))
