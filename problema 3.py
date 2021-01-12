768fa23f3b9c4e077244cbdc3f49d611fee750ee
moscvinandreea <70642112+moscvinandreea@users.noreply.github.com>
2021-01-12 22:47:00 +0200

    Add files via upload

---
+++ problema 3.py
@@ -0,0 +1,10 @@
+#Un brăduţ este împodobit cu globuleţe albe, roşii şi albastre. Numărul globuleţelor albe se citeşte din fişierul « globulet.txt ». Câte globuleţe are brăduţul, ştiind că numărul de globuleţe roşii este cu 3 mai mare decât numărul de globuleţe albe, iar globuleţele albastre sunt cu 2 mai puţine decât totalul celor albe şi roşii. Numărul total de globuleţe este înscris în fişierul « bradut.txt »
+#Exemplu: Date de intrare: 12 	Date de ieşire: 52.
+
+with open ("globulet.txt","r") as f:
+    a=f.readline()
+    r=int(a)+3
+    b=(int(a)+int(r))-2
+    s=int(a)+int(b)+int(r)
+with open ("bradut.txt","w") as f:
+    f.write(str(s))
